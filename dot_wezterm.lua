1650ba62c15778d8f4aa82f9aa319660f89d23ee
David Strauß <david.strauss@iqs.gv.at>
2023-06-13 12:35:08 +0200

    Add WezTerm configuration

---
+++ dot_wezterm.lua
@@ -0,0 +1,17 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font = wezterm.font 'JetBrains Mono'
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
+-- and finally, return the configuration to wezterm
+return config

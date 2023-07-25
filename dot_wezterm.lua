dc5a15d2ac3a17dfbd8fa209ce09321ec0d24166
David Strauß <david.strauss@iqs.gv.at>
2023-07-25 13:48:20 +0200

    Add color scheme to WezTerm configuration

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -12,6 +12,7 @@ end
 
 config.font = wezterm.font 'JetBrains Mono'
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.color_scheme = 'Solarized Light (Gogh)'
 
 -- and finally, return the configuration to wezterm
 return config

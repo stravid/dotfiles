4554821850eb2c5e555c35640b7496e9ecdc25d6
Strauß David <david.strauss@iqs.gv.at>
2024-06-18 13:47:28 +0200

    Make CTRL+c work like expected in a terminal setting
    When something is selected it copies the selection to the clipboard. Otherwise it sends CTRL+c so programs can be interrupted as usual.

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -200,12 +200,15 @@ config.keys = {
   {
     mods   = 'CTRL',
     key    = 'c',
-    action = wezterm.action.ActivatePaneDirection 'Down',
-  },
-  {
-    mods   = 'CTRL',
-    key    = 'v',
-    action = wezterm.action.CopyTo 'Clipboard',
+    action = wezterm.action_callback(function(window, pane)
+        selection_text = window:get_selection_text_for_pane(pane)
+        is_selection_active = string.len(selection_text) ~= 0
+        if is_selection_active then
+            window:perform_action(wezterm.action.CopyTo('ClipboardAndPrimarySelection'), pane)
+        else
+            window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
+        end
+    end),
   },
   {
     mods   = 'CTRL',

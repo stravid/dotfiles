3b04e71859e838467ab499cc9686455552221f3b
Strauß David <david.strauss@iqs.gv.at>
2024-06-20 14:27:16 +0200

    Make shortcuts better to use
    For application switching we use the left ALT key plus letters from the left side of the keyboard so the right hand can stay on the mouse.
    
    For Wezterm the LEADER was moved to `,` so it is easier to press.
    In addition the concept of key tables was introduced plus better key selection for certain actions.

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -15,7 +15,16 @@ config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
 config.debug_key_events = true
 config.disable_default_key_bindings = true
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = ',', mods = '', timeout_milliseconds = 1000 }
+
+-- Show which key table is active in the status area
+wezterm.on('update-right-status', function(window, pane)
+  local name = window:active_key_table()
+  if name then
+    name = 'TABLE: ' .. name
+  end
+  window:set_right_status(name or '')
+end)
 
 config.keys = {
   -- Reload configuration
@@ -32,40 +41,35 @@ config.keys = {
     action = wezterm.action.ShowDebugOverlay,
   },
 
-  -- Windows/Sessions
+  -- Workaround to make `,` key work in Wezterm.
+  -- It does require pressing `,` twice.
   {
     mods   = 'LEADER',
-    key    = 'w',
-    action = wezterm.action.PromptInputLine {
-      description = wezterm.format {
-        { Attribute = { Intensity = 'Bold' } },
-        { Foreground = { AnsiColor = 'Fuchsia' } },
-        { Text = 'Enter name for new workspace' },
-      },
-      action = wezterm.action_callback(function(window, pane, line)
-        -- line will be `nil` if they hit escape without entering anything
-        -- An empty string if they just hit enter
-        -- Or the actual line of text they wrote
-        if line then
-          window:perform_action(
-            wezterm.action.SwitchToWorkspace {
-              name = line,
-            },
-            pane
-          )
-        end
-      end),
-    },
+    key    = ',',
+    action = wezterm.action.SendString(','),
   },
+
+  -- Change key table so we can resize panes
   {
-    mods   = 'LEADER',
-    key    = 'q',
-    action = wezterm.action.ShowLauncherArgs {
-      flags = 'FUZZY|WORKSPACES',
+    key = 'r',
+    mods = 'LEADER',
+    action = wezterm.action.ActivateKeyTable {
+      name = 'resize_pane',
+      timeout_milliseconds = 5000,
+      one_shot = false,
     },
   },
-  -- Closing a workspace is currently not possible, see:
-  -- https://github.com/wez/wezterm/issues/3658
+
+  -- Change key table so we can access workspaces
+  {
+    key = 'w',
+    mods = 'LEADER',
+    action = wezterm.action.ActivateKeyTable {
+      name = 'workspaces',
+      timeout_milliseconds = 1000,
+      one_shot = false,
+    },
+  },
 
   -- Tabs
   {
@@ -147,51 +151,31 @@ config.keys = {
   },
   {
     mods   = 'LEADER',
-    key    = 'x',
+    key    = 'q',
     action = wezterm.action.CloseCurrentPane { confirm = false },
   },
   {
     mods   = 'LEADER',
-    key    = 'z',
+    key    = 'f',
     action = wezterm.action.TogglePaneZoomState
   },
   {
-    mods   = 'ALT',
-    key    = 'h',
-    action = wezterm.action.AdjustPaneSize { 'Left', 1 },
-  },
-  {
-    mods   = 'ALT',
-    key    = 'l',
-    action = wezterm.action.AdjustPaneSize { 'Right', 1 },
-  },
-  {
-    mods   = 'ALT',
-    key    = 'j',
-    action = wezterm.action.AdjustPaneSize { 'Down', 1 },
-  },
-  {
-    mods   = 'ALT',
-    key    = 'k',
-    action = wezterm.action.AdjustPaneSize { 'Up', 1 },
-  },
-  {
-    mods   = 'CTRL',
+    mods   = 'LEADER',
     key    = 'h',
     action = wezterm.action.ActivatePaneDirection 'Left',
   },
   {
-    mods   = 'CTRL',
+    mods   = 'LEADER',
     key    = 'l',
     action = wezterm.action.ActivatePaneDirection 'Right',
   },
   {
-    mods   = 'CTRL',
+    mods   = 'LEADER',
     key    = 'j',
     action = wezterm.action.ActivatePaneDirection 'Down',
   },
   {
-    mods   = 'CTRL',
+    mods   = 'LEADER',
     key    = 'k',
     action = wezterm.action.ActivatePaneDirection 'Up',
   },
@@ -217,10 +201,77 @@ config.keys = {
   },
 }
 
+config.key_tables = {
+  resize_pane = {
+    {
+      mods   = '',
+      key    = 'h',
+      action = wezterm.action.AdjustPaneSize { 'Left', 1 },
+    },
+    {
+      mods   = '',
+      key    = 'l',
+      action = wezterm.action.AdjustPaneSize { 'Right', 1 },
+    },
+    {
+      mods   = '',
+      key    = 'j',
+      action = wezterm.action.AdjustPaneSize { 'Down', 1 },
+    },
+    {
+      mods   = '',
+      key    = 'k',
+      action = wezterm.action.AdjustPaneSize { 'Up', 1 },
+    },
+
+    -- Cancel the mode by pressing escape
+    { key = 'Escape', action = 'PopKeyTable' },
+  },
+
+  workspaces = {
+    -- Windows/Sessions
+    {
+      mods   = '',
+      key    = 'n',
+      action = wezterm.action.PromptInputLine {
+        description = wezterm.format {
+          { Attribute = { Intensity = 'Bold' } },
+          { Foreground = { AnsiColor = 'Fuchsia' } },
+          { Text = 'Enter name for new workspace' },
+        },
+        action = wezterm.action_callback(function(window, pane, line)
+          -- line will be `nil` if they hit escape without entering anything
+          -- An empty string if they just hit enter
+          -- Or the actual line of text they wrote
+          if line then
+            window:perform_action(
+              wezterm.action.SwitchToWorkspace {
+                name = line,
+              },
+              pane
+            )
+          end
+        end),
+      },
+    },
+    {
+      mods   = '',
+      key    = 's',
+      action = wezterm.action.ShowLauncherArgs {
+        flags = 'FUZZY|WORKSPACES',
+      },
+    },
+    -- Closing a workspace is currently not possible, see:
+    -- https://github.com/wez/wezterm/issues/3658
+
+    -- Cancel the mode by pressing escape
+    { key = 'Escape', action = 'PopKeyTable' },
+  },
+}
+
 -- Wezterm / Tmux concept comparison:
 -- session = workspace
 -- window  = tab
 -- pane    = pane
 
--- and finally, return the configuration to wezterm
 return config
